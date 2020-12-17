b2823576c6127174147bb39f4e60515717ab2876
chinguyen21 <nguyen.thimai@northeastern.edu>
2020-12-17 10:47:47 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: %w(Non-Fiction Fiction), message: "%{value} is not a valid category"}
+  validate :valid_title?
+
+  def valid_title?
+    words = ["Won't Believe", "Secret", "Top", "Guess"]
+    if self.title.nil?
+      errors.add(:title, "Title cannot be empty")
+    elsif words.all? {|word| !self.title.include?(word) }
+      errors.add(:title, "Title should be sufficiently clickbait-y.")
+    end
+  end
 end
